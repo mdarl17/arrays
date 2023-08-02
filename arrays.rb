3b11e3fcc7d12904b2b88cb7932dc1e08d574239
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-02 17:21:43 -0600

    Add arrays to arrays.rb

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+afc_north = ["Browns", "Bengals", "Steelers", "Ravens"]
+win_prediction = [12, 9, 7, 10]  # Not based anywhere near reality
+win_pctg = [.422, .397, .632, .601]
+super_bowl_winners = [false, false, true, true]
