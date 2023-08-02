715117f2d1ca7f804a8ba852f19dc07e1e1f81f8
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-02 17:28:14 -0600

    Update win_pctg float array with values greater than 0

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,8 @@
 afc_north = ["Browns", "Bengals", "Steelers", "Ravens"]
 win_prediction = [12, 9, 7, 10]  # Not based anywhere near reality
-win_pctg = [.422, .397, .632, .601]
+win_pctg = [9.22, 7.397, 10.632, 8.601]
 super_bowl_winners = [false, false, true, true]
+
+# The <Array>.pop method will remove the last element of an array.
+# It will also return that value, allowing it to be stored in a new array,
+# or if used with 'puts' it will print that value to the terminal
