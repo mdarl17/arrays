d12b8284d4c02d6c769e45fbb8e550b36b740dfe
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-02 23:11:02 -0600

    Call pop, shift, push, unshift on arrays.

--- arrays.rb
+++ arrays.rb
@@ -5,4 +5,31 @@ super_bowl_winners = [false, false, true, true]
 
 # The <Array>.pop method will remove the last element of an array.
 # It will also return that value, allowing it to be stored in a new array,
-# or if used with 'puts' it will print that value to the terminal
+# or if used with 'puts' it will print that value to the terminal.
+
+puts "afc_north before pop => #{afc_north}"
+afc_north.pop
+puts "afc_north after pop called => #{afc_north}"
+lastEl_afc = afc_north.pop
+puts "lastEl_afc = afc_north.pop => #{lastEl_afc}\n\n"
+
+# The <Array>.shift method will remove the first element of an array.
+# Similar to <Array>.pop, this value will be returned.
+
+puts "win_prediction before shift => #{win_prediction}"
+win_prediction.shift
+puts "win_prediction after shift => #{win_prediction}"
+firstEl = win_prediction.shift
+puts "firstEl = win_prediction.shift => #{firstEl}\n\n"
+
+# The <Array>.push method will add the passed value to the end of the array on which the method is called.
+
+puts "win_ptcg before push => #{win_pctg}"
+win_pctg.push(12.282)
+puts "win_pctg after push => #{win_pctg}\n\n"
+
+# The <Array>.unshift method will add the passed value to the beginning of the array on which the method is called.
+
+puts "super_bowl_winners before unshift => #{super_bowl_winners}"
+super_bowl_winners.unshift(true)
+puts "super_bowl_winners after unshift => #{super_bowl_winners}"
